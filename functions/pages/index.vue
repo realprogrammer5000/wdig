9d358295256abd43add285abd444696015b5dce7
realprogrammer5000 <me@programmer5000.com>
2020-05-26 22:30:07 -0400

    Add Nuxt boilerplate

---
+++ functions/pages/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+      <div class="text-center">
+        <logo />
+      </div>
+      <v-card>
+        <v-card-title class="headline">
+          Test
+        </v-card-title>
+        <v-card-text>
+          Test
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            color="primary"
+            nuxt
+            to="/inspire"
+          >
+            Continue
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
