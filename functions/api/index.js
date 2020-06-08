0681ec652c70fd834893d4c7e8e512092c77ef41
realprogrammer5000 <me@programmer5000.com>
2020-06-08 01:31:43 -0400

    Create API

--- functions/api/index.js
+++ functions/api/index.js
@@ -1,57 +1,113 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const fs = require('fs')
+const path = require('path')
 const puppeteer = require('puppeteer')
+const lookup = require('safe-browse-url-lookup')({ apiKey: fs.readFileSync(path.join(__dirname, 'apikey')).toString().trim() })
+
 let browser
 
 app.use(cors())
-
-app.get('/lookup/:url', (req, res) => {
-  if (req.params.url.includes('err')) {
-    res.status(404).json({
-      results: [],
-      error: 'Not found'
-    })
-  } else {
-    res.json({
-      results: [
-        {
-          path: req.params.url
-        }
-      ]
-    })
-  }
-})
 
 app.get('/', (req, res) => {
   res.status(200).end('Hello, World')
 })
 
-const sleep = ms => new Promise(resolve => setTimeout(() => resolve(), ms));
+const sleep = ms => new Promise(resolve => setTimeout(() => resolve(), ms))
 
-app.get('/pptr', async (req, res) => {
-  if (!browser) {
-    browser = await puppeteer.launch({ args: ['--no-sandbox'] })
+const analyseUrl = async (rawUrl) => {
+  const results = []
+  const url = new URL(rawUrl)
+  let hostname = url.hostname
+  if (hostname.endsWith('.')) { hostname = hostname.slice(0, -1) }
+  if (hostname.endsWith('.latlmes.com') || hostname === 'latlmes.com') { results.push({ type: 'rickroll' }) }
+  if (url.hostname.endsWith('.youtube.com') && url.pathname.startsWith('/watch')) {
+    const video = url.searchParams.get('v')
+    if ('dQw4w9WgXcQ,j5a0jTc9S10,dPmZqsQNzGA,EE-xtCF3T94,V-_O7nl0Ii0,xfr64zoBTAQ,Uj1ykZWtPYI,ID_L0aGI9bg,REWyCy_m39Q,nHRbZW097Uk,nQGsT44rVjk,VSa2IqDwnQ8,kJmKBhYsJho,Dh-CW22axyY,0EY98EsPXs8,-lmhJOhQHWc,3KANI2dpXLw,06pBzAlItwI,cH1NLToRz_o,gidHrKbTpog,0Z0i-nimHkU,OYpwAtnywTk,fZi4JxbTwPo,BXuxOeg6PiQ,6RyOsIseJbk,klqi_h9FElc,ObUZAcLgDn8,Eg7XtrH0duI,iQAAZX1gxvo,y6120QOlsfU,0SoNH07Slj0,cqF6M25kqq4,s99hC5WUwjo,lHyeAtiiL18,fMnIpIMuBJI,vkbQmH5MPME,QdNEtVH9bZE,PibwQR9nRus,d4N5LnsNpF0,DIs7dtlMCP0,Q53-mWHvxJo,cAN30xJp2Cs,_sWG4uhLiWo,KXPP2XDYpjQ,8O_ifyIIrN4,SWejvNI7WlM,N3MKlU5_Gs0,kczWpSN6emg,JU7jgF_dktI'.split(',').includes(video)) { results.push({ type: 'rickroll' }) }
   }
 
-  const page = await browser.newPage()
-  await page.setDefaultNavigationTimeout(10000)
-  page.on('load', () => console.log('load', page.url()))
-  page.on('domcontentloaded', () => console.log('domcontentloaded'))
-  // const prom = page.goto('http://httpbin.org/redirect-to?url=https://httpbin.org/get')
-  page.goto('http://v.ht/toast')
   try {
-    await page.waitForNavigation({ waitUntil: 'networkidle2' })
+    if (await lookup.checkSingle(rawUrl)) { results.push({ type: 'safebrowsing' }) }
   } catch (e) {
-    console.log(e)
+    console.error(e)
   }
 
-  console.log('navigated')
+  return results
+}
 
-  await sleep(500)
+app.get('/lookup/:url', async (req, res) => {
+  const errors = []
+  let page
+  let hasFinished = false
+  try {
+    if (!browser) {
+      browser = await puppeteer.launch({
+        args: ['--no-sandbox'],
+        defaultViewport: {
+          width: 1920,
+          height: 1080
+        }
+      })
+    }
 
-  res.status(200).send(await page.content())
+    page = await browser.newPage()
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36')
+    await page.setDefaultNavigationTimeout(2000)
+  } catch (e) {
+    console.error(e)
+    return res.status(500).end('Internal server error')
+  }
+
+  const originalUrl = req.params.url
+  try {
+    // eslint-disable-next-line no-new
+    new URL(originalUrl)
+  } catch (e) {
+    console.error(e)
+    return res.status(400).end('Invalid URL')
+  }
+
+  res.status(200).setHeader('Content-Type', 'application/json')
+
+  page.on('load', () => {
+    if (!hasFinished) {
+      const url = page.url()
+      // if (originalUrl !== url) {
+      res.write(JSON.stringify({ type: 'redirect', to: url }) + '\n')
+      // }
+      console.log('url', url)
+    }
+  })
+  try {
+    await page.goto(originalUrl)
+  } catch (e) {
+    if (e.message.startsWith('net::ERR_TOO_MANY_REDIRECTS ')) {
+      errors.push('TOO_MANY_REDIRECTS')
+    } else if (e.message.startsWith('net::ERR_ABORTED ')) {
+      errors.push('ABORTED')
+    } else {
+      console.error('page load error', e.message)
+    }
+  }
+
+  console.log('page loaded')
+
+  await sleep(1500)
+
+  hasFinished = true
+  const finalUrl = await page.url()
+  res.write(JSON.stringify({ type: 'analysis', analysis: await analyseUrl(finalUrl) }) + "\n")
+  if (page.url().startsWith('chrome-error://')) {
+    res.send({ failed: true })
+  } else {
+    const dataUri = `data:image/png;base64,${await page.screenshot({ encoding: 'base64' })}`
+    res.write(JSON.stringify({ type: 'screenshot', dataUri, url: await page.url(finalUrl) }))
+    res.end()
+  }
+  // const buffer = await page.screenshot()
   await page.close()
+  console.log(errors)
 })
 
 module.exports = app
