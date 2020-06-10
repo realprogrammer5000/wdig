8943ae2433a023c42137c76afedc778b47194087
realprogrammer5000 <me@programmer5000.com>
2020-06-10 00:48:04 -0400

    Simplify main layout

--- functions/layouts/default.vue
+++ functions/layouts/default.vue
@@ -4,6 +4,9 @@
       v-model="drawer"
       :mini-variant="miniVariant"
       :clipped="clipped"
+      width="auto"
+      floating
+      disable-resize-watcher
       fixed
       app
     >
@@ -30,43 +33,40 @@
       app
     >
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
+      <!--<v-btn
         icon
         @click.stop="miniVariant = !miniVariant"
       >
         <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
+      </v-btn>-->
+      <!--<v-btn
         icon
         @click.stop="clipped = !clipped"
       >
         <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
+      </v-btn>-->
       <v-toolbar-title v-text="title" />
-      <v-spacer />
+      <!--<v-spacer />
       <v-btn
         icon
         @click.stop="rightDrawer = !rightDrawer"
       >
         <v-icon>mdi-menu</v-icon>
       </v-btn>
+      -->
     </v-app-bar>
     <v-content>
       <v-container>
         <nuxt />
+        <!--        <nuxt keep-alive/>-->
       </v-container>
     </v-content>
-    <v-navigation-drawer
+    <!--<v-navigation-drawer
       v-model="rightDrawer"
       :right="right"
       temporary
       fixed
+      app
     >
       <v-list>
         <v-list-item @click.native="right = !right">
@@ -78,11 +78,8 @@
           <v-list-item-title>Switch drawer (click me)</v-list-item-title>
         </v-list-item>
       </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :fixed="fixed"
-      app
-    >
+    </v-navigation-drawer>-->
+    <v-footer>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
@@ -92,25 +89,24 @@
 export default {
   data () {
     return {
-      clipped: false,
+      clipped: true,
       drawer: false,
-      fixed: false,
       items: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
+          icon: 'mdi-magnify',
+          title: 'Lookup',
           to: '/'
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
+          icon: 'mdi-help-circle',
+          title: 'Help',
+          to: '/help'
         }
       ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'Where Does It Go?'
     }
   }
 }
