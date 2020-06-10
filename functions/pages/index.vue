652b89877455b962251291de45690f46ab82f239
realprogrammer5000 <me@programmer5000.com>
2020-06-10 18:26:39 -0400

    Fix enter submitting form

--- functions/pages/index.vue
+++ functions/pages/index.vue
@@ -4,14 +4,18 @@
     justify-center
     align-center
   >
-    <v-text-field v-model="url" label="URL to search" />
-    <v-btn
-      color="primary"
-      nuxt
-      :to="linkUrl"
-    >
-      Continue
-    </v-btn>
+    <v-form @submit.prevent="submit">
+      <v-text-field v-model="url" type="url" :rules="[validate]" label="URL to search" />
+      <v-btn
+        ref="submitBtn"
+        color="primary"
+        nuxt
+        :to="linkUrl"
+        type="submit"
+      >
+        Continue
+      </v-btn>
+    </v-form>
   </v-layout>
 </template>
 <script>
@@ -22,6 +26,22 @@ export default {
   computed: {
     linkUrl () {
       return `/url/${encodeURIComponent(this.url)}`
+    }
+  },
+  methods: {
+    submit () {
+      this.$router.push({
+        path: this.linkUrl
+      })
+    },
+    validate: (url) => {
+      try {
+        // eslint-disable-next-line no-new
+        new URL(url)
+        return true
+      } catch (e) {
+        return 'Invalid URL'
+      }
     }
   }
 }
