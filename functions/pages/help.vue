30bb0801625ae6bba13e24ef939a6605bdc1039b
realprogrammer5000 <me@programmer5000.com>
2020-06-11 15:08:16 -0400

    Add .

--- functions/pages/help.vue
+++ functions/pages/help.vue
@@ -7,7 +7,7 @@
     <div class="text-center help-page">
       <h1>Help</h1>
       <p>Where Does It Go? lets you find out where a URL goes without actually going there. It runs a real web browser and shows you everywhere it goes, including a screenshot of the final destination. It also warns you if the final destination is a rickroll or flagged as unsafe by <a href="https://safebrowsing.google.com/">Google Safe Browsing</a>.</p>
-      <p>WDIG is open source <a herf="https://github.com/realprogrammer5000/wdig">on Github</a></p>
+      <p>WDIG is open source <a herf="https://github.com/realprogrammer5000/wdig">on Github</a>.</p>
     </div>
   </v-layout>
 </template>
